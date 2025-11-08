e56b3afe974a3f9282941c2ab1b05b326c6eea84
Omkar <omkar19jagtap@gmail.com>
2025-11-08 19:30:13 +0530

    Starting Today

---
+++ VeryBasic.py
@@ -0,0 +1,13 @@
+print("I'm Gonna be the best out there!");
+
+#variable = A container for a value (String,Integer,float,boolean)
+#           A variable behaves as if it was value it contains
+
+First_name = "BikyaJackstron"
+Food = "Biryani"
+email = "bokya19omkar@gmail.com"
+
+print(First_name)
+print(f"Hello {First_name}")       
+print(f"You Like {Food}")
+print(f"Your email is {email}");
