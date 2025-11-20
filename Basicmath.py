806eeefe714e0d7b6a63d1181a5c58d44a051ca4
Omkar <omkar19jagtap@gmail.com>
2025-11-20 14:21:25 +0530

    covered basics

---
+++ Basicmath.py
@@ -0,0 +1,24 @@
+import math
+
+#x=9.9
+#print(math.pi)
+#print(math.e)
+#print(math.sqrt(x))
+#print(math.ceil(x))
+#print(math.floor(x))
+
+#Find Circumference of a circle
+#r = float(input("Enter the radius: "))
+#result = 2*math.pi*r
+#print(f"Circumference of circle is {round(result, 2)}cm")
+
+#Area of a circle
+#r = float(input("Enter radius of a circle: "))
+#result = math.pi*pow(r,2)
+#print(f"Area of circle is {round(result, 2)}cm")
+
+#Hypotenuse of a right triangle
+#a = float(input("Enter Base of triangle (cm): "))
+#b = float(input("Enter height of triangle: "))
+#result = math.sqrt(pow(a,2) + pow(b,2))
+#print(f"Hypotenuse is {round(result,2)}")
---
+++ Conditions&loops.py
@@ -0,0 +1,156 @@
+#if = So some code only if some condition is true else do something else
+'''
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are now signed up")
+elif age <= 0:
+    print("You haven't even born yet!")
+else:
+    print("Grow up lil bro")
+
+
+#response = input("Would you like food (Y/N)? ")
+#if response.lower() == "y":
+#    print("Then Eat Dumbo!")
+#elif response.lower() == "n":
+#    print("Then don't eat dumbo!")
+#else:
+#    print("Choose something valid")
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter first operand: "))
+num2 = float(input("Enter second operand: "))
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print("Enter valid operator")
+print(f"You choose '{operator}' and your answer is {result}")
+
+
+#Python Weight Converter
+weight = float(input("Enter you Weight: "))
+unit = input("Kilogram or Pounds? (K or L): ")
+
+if unit.lower() == "k":
+    weight = weight * 2.205
+    unit = "Lbs."
+elif unit.lower() == "l":
+    weight = weight / 2.205
+    unit = "Kgs."
+else:
+    print(f"{unit} was not valid")
+
+print(f"Your weight is: {round(weight,2)} {unit}")
+
+#temp converter
+unit = input("Is this temp is Celsius or Fahrenheit (C/F): ")
+temp = float(input("Enter the temp: "))
+
+if unit.lower() == "c":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temp in Fahrenheit is: {temp}°F")
+elif unit.lower() == "f":
+    temp = round((temp - 32) * 5/9, 1)
+    print(f"The temp in Celsius is: {temp}°C")
+else:
+    print(f"{unit} is not a valid unit")
+
+
+# Logical operators = evaluate multiple conditions (or, and, not)
+# or = at least one condition must be True
+# and = both conditions must be True
+# not = inverts the conditions (not False, not True)
+
+temp = 25
+is_raining = False
+
+if temp > 35 or temp < 0 or is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled")
+
+
+#conditional expressions = A one-line shortcut for the if-else statement (ternary operator)
+#                          Print or assign one of two values based on a condition
+#                          x if condition else y
+
+num = 6
+# print("positive" if num > 0 else "negative")
+result = "EVEN" if num % 2 == 0 else "ODD"
+print(result)
+
+
+#String methods
+#name = input("enter your full name: ")
+phone_number = input("Enter your phone number: ")
+#result = len(name)
+#result = name.find(" ")
+#result = name.rfind("o")  #r means reverse
+#result = name.capitalize()
+#result = name.upper()
+#result = name.lower()
+#result = name.isdigit()
+#result = phone_number.count("-")
+result = phone_number.replace("-" , " ")
+print(result)
+help(str)   # gives all string methods
+
+
+#execise - validate user input exercise
+#1. username is no more than 12 characters
+#2. username must not contain spaces
+#3. username must not contain digits
+
+username = input("Enter your username: ")
+if len(username) > 12:
+    print("username should not be more than 12 characters")
+elif not username.find(" ") == -1:
+    print("Username should not contain any spaces")
+elif not username.isalpha():
+    print("Username should not contain any digits")
+else:
+    print(f"Welcome {username}")
+
+
+#indexing = accessing elements of a sequence using [] (indexing operator)
+#           [start : end : step]
+
+#credit_number = "1234-5678-9012-3456"
+#last_digits = credit_number[-4:]
+#print(credit_number[0])
+#print(credit_number[0 : 4])   
+#print(credit_number[ : 4])
+#print(credit_number[5 :])
+#print(credit_number[-1])
+#print(credit_number[::2])
+#print(credit_number[::-1])
+#print(f"XXXX-XXXX-XXXX-{last_digits}")
+'''
+
+#format specifier = { value : flags} format a value based on what flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# := = plce sign to leftmost position
+# :  = insert a space before positive numbers
+# :, = comma separator
+
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+
+#print(f"price 1 is {price1 : .2f}")
+print(f"price 2 is {price2 : 10}")
+print(f"price 3 is {price3 : .3f}")
